af7916692047ee43be5750d9603f2fa5a226777a
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2023-10-24 07:58:21 +0600

    add test for course

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .services import CourseService
 
-# Create your tests here.
+class CourseServiceTestCase(TestCase):
+    def test_get_courses(self):
+        # Test the get_courses method
+        courses = CourseService.get_courses()
+        self.assertEqual(len(courses), 0)  # Assuming no courses are initially present
+
+    def test_create_course(self):
+        # Test the create_course method
+        course = CourseService.create_course('Test Course', 'Test Description', 'Test Instructor', 60, 29.99)
+        self.assertEqual(course.title, 'Test Course')
+        self.assertEqual(course.description, 'Test Description')
+        self.assertEqual(course.instructor, 'Test Instructor')
+        self.assertEqual(course.duration, 60)
+        self.assertEqual(course.price, 29.99)
+
+    def test_filter_courses(self):
+        # Test the filter_courses method
+        filtered_courses = CourseService.filter_courses(instructor='Test Instructor', price=29.99, duration=60)
+        self.assertIsNotNone(filtered_courses)
