6cd4872de1864caf4a77d490b73103fa365a4e9e
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2023-10-24 09:39:27 +0600

    update urls

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CourseListCreateAPIView, CourseDetailAPIView
 
 urlpatterns = [
-    path('courses/', CourseListCreateAPIView.as_view(), name='course-list-create'),
-    path('courses/<int:pk>/', CourseDetailAPIView.as_view(), name='course-detail')
+    path('courses', CourseListCreateAPIView.as_view(), name='course-list-create'),
+    path('courses/<int:pk>', CourseDetailAPIView.as_view(), name='course-detail')
 ]
--- enrollments/urls.py
+++ enrollments/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import EnrollmentListCreateAPIView
 
 urlpatterns = [
-    path('enrollments/', EnrollmentListCreateAPIView.as_view(), name='enrollment-list-create'),
+    path('enrollments', EnrollmentListCreateAPIView.as_view(), name='enrollment-list-create'),
 ]
