0f7bd73688ae958e80fbb42a30430cce373c5daa
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2023-10-24 08:11:45 +0600

    test case for enrollment

--- enrollments/tests.py
+++ enrollments/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .services import EnrollmentService
 
-# Create your tests here.
+class EnrollmentServiceTestCase(TestCase):
+    def test_enroll_student(self):
+        # Test the enroll_student method
+        enrollment = EnrollmentService.enroll_student('John Doe', 1, '2023-10-01')
+        self.assertIsNotNone(enrollment)
+
+    def test_validate_enrollment_invalid(self):
+        # Test the validate_enrollment method for an invalid case
+        is_valid = EnrollmentService.validate_enrollment('', 1)
+        self.assertFalse(is_valid)
